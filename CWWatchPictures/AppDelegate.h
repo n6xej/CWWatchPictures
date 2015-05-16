0dbc889b10629296af8e1a5c8b59824fb47fbbc9
n6xej <cworley@Christophers-MacBook-Pro.local>
2015-05-15 22:48:03 -0600

    Initial Commit

---
+++ CWWatchPictures/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CWWatchPictures
+//
+//  Created by Christopher Worley on 5/15/15.
+//  Copyright (c) 2015 Christopher Worley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
